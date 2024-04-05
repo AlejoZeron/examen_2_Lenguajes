ff7a7c3b9da991343230c13b1c5a3b08484b2fd6
Alejandro Zeron <zeron@MacBook-Air-de-Alejandro.local>
2024-04-05 15:57:09 -0600

    First commit

---
+++ src/main/java/hn/unah/lenguajes/Examen2/demo/Examen2LengaujesApplication.java
@@ -0,0 +1,13 @@
+package hn.unah.lenguajes.Examen2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen2LengaujesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen2LengaujesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hn/unah/lenguajes/Examen2/demo/Examen2LengaujesApplicationTests.java
@@ -0,0 +1,13 @@
+package hn.unah.lenguajes.Examen2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen2LengaujesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
